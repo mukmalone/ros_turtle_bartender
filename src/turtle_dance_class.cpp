ebc9a3616790d0583c2fe4d72c83d988eb199ba2
mukmalone <michael.muldoon.home@gmail.com>
2020-10-31 23:22:01 -0400

    Turtule_dance_class now fully working

--- src/turtle_dance_class.cpp
+++ src/turtle_dance_class.cpp
@@ -4,39 +4,68 @@
 #include <turtlesim/Spawn.h>
 #include <turtlesim/SetPen.h>
 #include <turtlesim/Pose.h>
+#include <geometry_msgs/Twist.h>
+#include <ros_turtle_bartender/NextGoal.h>
 
 using namespace std;
-float x,y,theta;
+
+const float pi = 3.14159265358979323846;
 
 class Robot_Class {
 	public:	
 		Robot_Class() {
 			cout<<"Hello World"<<endl;
-
-			//pose for each Pary_Turtle position in Go to Goal algorithm
-			//ros::Subscriber sub_pose = n.subscribe<turtlesim::Pose>(robot_name + "/pose", 5, &Robot_Class::poseCallback, this);		
 		}
+
 		string robot_name;
+		ros::NodeHandle n;
+		ros::Subscriber subscriber_pose;
+		turtlesim::Pose pose;
+    	ros::Publisher cmd_vel;    
+		geometry_msgs::Twist control_command;
+		float goal_x;
+		float goal_y;
 
 		void move_robot();
 
 		void stop_robot();
 
-		ros::NodeHandle n;
-
-		ros::Subscriber sub_pose;
-
-		void spawn_robot(ros::NodeHandle n);
+		void spawn_robot();
 
 		void poseCallback(const turtlesim::Pose::ConstPtr& msg);
+
+		void get_goal();
+
+		bool robot_at_goal();
 
 };
 
 void Robot_Class::move_robot()
 {
+	float angle = atan2(goal_y-pose.y, goal_x-pose.x) - pose.theta;
+    //ensure angle is between -pi and pi
+    if (angle<-pi){
+        angle+=2*pi;
+    }
+    else if (angle>pi){
+        angle-=2*pi;
+    }
 
-	cout<<"Moving Robot"<<endl;
+	//angular velocity
+	control_command.angular.z = 5 * (angle); 
+	float adaptive_control=1.0;
 
+	//control to enable sharp turns and staighter paths
+	if(abs(control_command.angular.z)<.5){
+		adaptive_control=.5;
+	} else {
+		adaptive_control=abs(control_command.angular.z);
+	}
+
+	//linear velocity
+	control_command.linear.x = .5/adaptive_control* sqrt( pow(goal_x-pose.x,2) + pow(goal_y-pose.y,2) );
+
+	cmd_vel.publish(control_command);
 }
 
 void Robot_Class::stop_robot()
@@ -45,7 +74,7 @@ void Robot_Class::stop_robot()
 	cout<<"Stopping Robot"<<endl;
 }
 
-void Robot_Class::spawn_robot(ros::NodeHandle n)
+void Robot_Class::spawn_robot()
 {
 	//This class will spawn the turtle and turn-off the pen
 	turtlesim::Spawn turtle;    
@@ -61,17 +90,47 @@ void Robot_Class::spawn_robot(ros::NodeHandle n)
     ros::ServiceClient pen = n.serviceClient<turtlesim::SetPen>("/" + robot_name + "/set_pen");
     pen.call(pen_state);
 
-	sub_pose = n.subscribe<turtlesim::Pose>("/" + robot_name + "/pose", 5, &Robot_Class::poseCallback, this);		
-	cout<<"Robot spawned "<<robot_name<<endl;
+	subscriber_pose = n.subscribe<turtlesim::Pose>("/" + robot_name + "/pose", 5, &Robot_Class::poseCallback, this);
+	cmd_vel = n.advertise<geometry_msgs::Twist>(robot_name+"/cmd_vel", 10);
+
+	//initiate the values of the control command to zero where needed
+	control_command.linear.y = 0.0;
+    control_command.linear.z = 0.0;
+    control_command.angular.x = 0.0;
+    control_command.angular.y = 0.0;
 }
 
 void Robot_Class::poseCallback(const turtlesim::Pose::ConstPtr& msg)
 {
-    x = msg->x;
-    y = msg->y;
-    theta = msg->theta;
-	cout<<"hello this is pose callback for: "<<robot_name<<endl;
-	//cout<<"x: "<<x<<endl;
+    pose.x=msg->x;
+	pose.y=msg->y;
+	pose.theta=msg->theta;
+}
+
+void Robot_Class::get_goal()
+{
+	ros::ServiceClient goalClient = n.serviceClient<ros_turtle_bartender::NextGoal>("/next_goal");
+    ros_turtle_bartender::NextGoal nextGoal;  
+	nextGoal.request.x=pose.x;
+    nextGoal.request.y=pose.y;
+    nextGoal.request.theta=pose.theta;
+    goalClient.call(nextGoal);
+	goal_x=nextGoal.response.x;
+	goal_y=nextGoal.response.y;
+}
+
+bool Robot_Class::robot_at_goal()
+{
+	//check if we are at the goal within tolerance
+    float dx, dy, tolerance=0.1;
+    dx = abs(pose.x-goal_x);
+    dy = abs(pose.y-goal_y);
+	if(dx<=tolerance && dy<=tolerance){
+        //if we are at the goal move to next goal
+        return true;
+    } else {
+		return false;
+	}
 }
 
 int main(int argc, char **argv)
@@ -83,20 +142,34 @@ int main(int argc, char **argv)
     ros::service::waitForService("/spawn");
 	
 	//spawn all the party robots
-	int num_robots = 4;
+	int num_robots = 14;
 	Robot_Class robot[num_robots];
 
+	//spawn all the party turtles and initiate their first dance
 	for(int i=0; i<num_robots; i++){
 		robot[i].robot_name = "Party_Turtle_" + to_string(i);
-		robot[i].spawn_robot(n);
-		//robot[i].move_robot();
-		//robot[i].stop_robot();
+		robot[i].spawn_robot();
+		robot[i].get_goal();
 	}		
-	 
+
+	bool test = robot[0].robot_at_goal();
+
+	cout<<to_string(test)<<endl;
+
 	ros::Rate loop_rate(20);
 
 	while (ros::ok())
     {   
+		//cycle through each party turtle.  if it is at the goal, get a new one
+		//if it is not, keep moving towards the goal
+		for(int i = 0 ;i<num_robots;i++){
+			if(!robot[i].robot_at_goal()){
+				robot[i].move_robot();
+			} else {
+				robot[i].get_goal();
+			}
+		}
+
 		ros::spinOnce();
         loop_rate.sleep();
 	}
