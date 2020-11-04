1a6c8a563c2438bf58ff0ce50bc7eb06c913d210
mukmalone <michael.muldoon.home@gmail.com>
2020-11-04 18:36:20 -0500

    Integrating curl

--- src/drink_order_server.cpp
+++ src/drink_order_server.cpp
@@ -1,5 +1,12 @@
 #include <ros/ros.h>
 #include <ros_turtle_bartender/DrinkOrder.h>
+#include <stdio.h>
+#include <curl/curl.h>
+#include <curlpp/cURLpp.hpp>
+
+#include <curlpp/Easy.hpp>
+#include <curlpp/Options.hpp>
+
 using namespace std;
 
 bool get_next_order(ros_turtle_bartender::DrinkOrder::Request &req,
@@ -19,6 +26,19 @@ int main (int argc, char **argv)
 
     ros::ServiceServer server = n.advertiseService("/next_order", 
         get_next_order);
+
+    // HTTP response body (not headers) will be sent directly to this stringstream
+    stringstream response;
+
+    curlpp::Easy foo;
+    foo.setOpt( new curlpp::options::Url( "http://localhost:3000/" ) );
+    foo.setOpt( new curlpp::options::UserPwd( "blah:passwd" ) );
+    foo.setOpt( new curlpp::options::WriteStream( &response ) );
+
+    // send our request to the web server
+    foo.perform();
     
     ros::spin();
+
 }
+
