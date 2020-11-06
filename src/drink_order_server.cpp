9aa8db73fe32320ce25ecff8d90b5de7e1fb39e0
mukmalone <michael.muldoon.home@gmail.com>
2020-11-05 20:00:17 -0500

    working on posting data to webserver

--- src/drink_order_server.cpp
+++ src/drink_order_server.cpp
@@ -2,21 +2,51 @@
 #include <ros_turtle_bartender/DrinkOrder.h>
 #include <stdio.h>
 #include <curl/curl.h>
-#include <curlpp/cURLpp.hpp>
-#include <curlpp/Easy.hpp>
-#include <curlpp/Options.hpp>
+#include <iostream>
+#include <string>
+
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
 
 using namespace std;
 
 bool get_next_order(ros_turtle_bartender::DrinkOrder::Request &req,
                     ros_turtle_bartender::DrinkOrder::Response &res)
 {
-    res.customer = "Party_Turtle_1";
+    CURL *curl;
+    CURLcode res_curl;
+    string readBuffer;
+    string delimiter = ",";
+    string strArguments = "name=daniel&project=curl";
+    const char *data = "data";;
+    
+    curl = curl_easy_init();
+    
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_POST, 1L);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, 12L);
+        
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
+        curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:3000");
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        res_curl = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+
+        res.customer = readBuffer.substr(0, readBuffer.find(delimiter));
+        res.drink = readBuffer.substr(readBuffer.find(delimiter), readBuffer.length());
+    }
+    
     res.drink = "Red Wine";
     
     cout<<"Customer: "<<res.customer<<"Drink: "<<res.drink<<endl;
     return true;
 }
+
+
 
 int main (int argc, char **argv)
 {
@@ -25,18 +55,7 @@ int main (int argc, char **argv)
 
     ros::ServiceServer server = n.advertiseService("/next_order", 
         get_next_order);
-
-    // HTTP response body (not headers) will be sent directly to this stringstream
-    stringstream response;
-
-    curlpp::Easy foo;
-    foo.setOpt( new curlpp::options::Url( "http://localhost:3000/" ) );
-    foo.setOpt( new curlpp::options::UserPwd( "blah:passwd" ) );
-    foo.setOpt( new curlpp::options::WriteStream( &response ) );
-
-    // send our request to the web server
-    foo.perform();
-    
+  
     ros::spin();
 
 }
--- webserver/server.js
+++ webserver/server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Party_Turtle_1,Red Wine')
 })
 
 app.listen(port, () => {
